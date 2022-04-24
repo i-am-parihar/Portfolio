eff07fcf18e48cd36a0bc8252b40b0fc5d54cef2
i-am-parihar <paritoshparihar4@gmail.com>
2022-04-24 20:06:33 +0530

    Redux add

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import { SiRedux} from "react-icons/si" ;
 import {
   DiJavascript1,
   DiReact,
@@ -11,10 +12,13 @@ function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-      <span> < DiJavascript1/><span><h6>JavaScript</h6></span></span> 
+      <span> < DiReact/><span><h6>React</h6></span></span> 
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-      <span> < DiReact/><span><h6>React</h6></span></span> 
+      <span> < SiRedux/><span><h6>Redux</h6></span></span> 
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <span> < DiJavascript1/><span><h6>JavaScript</h6></span></span> 
       </Col>
       <Col xs={4} md={2} className="tech-icons">
       <span> < DiHtml5/><span><h6>HTML</h6></span></span> 
